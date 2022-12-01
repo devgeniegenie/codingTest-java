7cea935c44c9c227de2a73b7e2bfc60aa040dffd
Genie <devgeniegenie@gmail.com>
2022-12-01 23:45:15 +0900

    1,2차원 배열 12번 : 멘토링

--- src/array/Array12.java
+++ src/array/Array12.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Array12 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System .in);
         int n = scanner.nextInt(), m = scanner.nextInt();
         int[][] input = new int[m][n];
         for (int i = 0; i <m; i++) {
@@ -16,6 +16,21 @@ public class Array12 {
     }
 
     static int solution(int[][] input, int n, int m) {
-
+        int result = 0;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++){
+                int cnt = 0;
+                for(int k = 0; k < m; k++){
+                    int pi = 0, pj = 0;
+                    for(int s = 0; s < n; s++){
+                        if(input[k][s] == i) pi = s;
+                        if(input[k][s] == j) pj = s;
+                    }
+                    if(pi < pj) cnt++;
+                }
+                if(cnt == m) result ++;
+            }
+        }
+        return result;
     }
 }
