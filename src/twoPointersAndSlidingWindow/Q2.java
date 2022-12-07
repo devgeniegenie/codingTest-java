7d60e1037d581d82c192f552756d935cefe9171d
Genie <devgeniegenie@gmail.com>
2022-12-07 20:36:35 +0900

    Two Pointers, Sliding window 2번 : 공통원소 구하기

--- src/twoPointersAndSlidingWindow/Q2.java
+++ src/twoPointersAndSlidingWindow/Q2.java
@@ -1,8 +1,6 @@
 package twoPointersAndSlidingWindow;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Q2 {
     public static void main(String[] args) {
@@ -18,13 +16,48 @@ public class Q2 {
             input2[i] = scanner.nextInt();
         }
 
-        for (int i : mySolution(input1, input2, n, m)) {
+        for (int i : solution(input1, input2, n, m)) {
             System.out.print(i + " ");
         }
     }
 
-    static ArrayList<Integer> mySolution(int[] input1, int[] input2, int n, int m) {
+    static List<Integer> mySolution(int[] input1, int[] input2, int n, int m) {
         List<Integer> result = new ArrayList<>();
+        for(int x : input1) {
+            for(int y = 0; y < m; y++) {
+                if(x == input2[y]) {
+                    result.add(x);
+                    input2[y] = 0;
+                }
+            }
+        }
+        for(int x : input2) {
+            for(int y : input1) {
+                if(x == y) {
+                    result.add(x);
+                }
+            }
+        }
+        result.sort(Comparator.naturalOrder());
+        return result;
+    }
 
+    static List<Integer> solution(int[] input1, int[] input2, int n, int m) {
+        List<Integer> result = new ArrayList<>();
+        Arrays.sort(input1);
+        Arrays.sort(input2);
+        int p1 = 0, p2 = 0;
+
+        while(p1 < n && p2 < m) {
+            if(input1[p1] == input2[p2]){
+                result.add(input1[p1++]);
+                p2++;
+            } else if(input1[p1] < input2[p2]) {
+                p1 ++;
+            } else {
+                p2 ++;
+            }
+        }
+        return result;
     }
 }
